bf4ed20d9374888b992bb467ea83b90998e92a43
Melissa <melissaforonda06@gmail.com>
2025-09-02 21:29:24 -0500

    Agrego Tarea 3 home del insta-pic

---
+++ udemed_202502/Talleres/ANGULAR/insta-pic-app/src/app/app.routes.ts
@@ -0,0 +1,26 @@
+import { Routes } from '@angular/router';
+import { Login } from './features/pages/login/login';
+import { SignUp } from './features/pages/sign-up/sign-up';
+import { Home } from './features/pages/home/home';
+
+export const routes: Routes = [
+    {
+        path:"",
+        component:Login,
+        pathMatch:"full"
+    },
+    {
+        path:"sign-up",
+        component:SignUp,
+        pathMatch:"full"
+    },
+    {
+        path:"home",
+        component:Home,
+        pathMatch:"full"
+    },
+    {
+        path:"**",
+        redirectTo:""
+    }
+];
